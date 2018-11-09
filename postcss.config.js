331004ec18f2764a840524cabd3c1b35c202cabf
Velichko Konstantin <zoxon.box@gmail.com>
2018-11-09 13:52:39 +0600

    Add cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,33 @@
-module.exports = {
-  plugins: {
-    autoprefixer: {
-      flexbox: "no-2009",
-      cascade: false,
-      grid: true
-    },
-    "postcss-flexbugs-fixes": {},
-    perfectionist: {
-      cascade: false,
-      colorCase: "lower",
-      colorShorthand: true,
-      format: "expanded",
-      indentChar: " ",
-      indentSize: 2,
-      trimLeadingZero: false,
-      trimTrailingZeros: true,
-      zeroLengthNoUnit: true
-    },
-    "postcss-reporter": {}
-  }
+module.exports = ctx => {
+  const cssNanoDefaults = {
+    preset: "default"
+  };
+  const {
+    env = "development",
+    options: { minify = cssNanoDefaults }
+  } = ctx;
+
+  return {
+    plugins: {
+      autoprefixer: {
+        flexbox: "no-2009",
+        cascade: false,
+        grid: true
+      },
+      "postcss-flexbugs-fixes": {},
+      perfectionist: {
+        cascade: false,
+        colorCase: "lower",
+        colorShorthand: true,
+        format: "expanded",
+        indentChar: " ",
+        indentSize: 2,
+        trimLeadingZero: false,
+        trimTrailingZeros: true,
+        zeroLengthNoUnit: true
+      },
+      cssnano: env === "production" ? minify : false,
+      "postcss-reporter": {}
+    }
+  };
 };
